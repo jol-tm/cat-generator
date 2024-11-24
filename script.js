a73e8118b278f34271380cdb112793b0013af991
jol-tm <joaolucastmagalhaes@gmail.com>
2024-11-24 09:15:07 -0300

    Animação só roda quando carregar a imagem, garantindo que sempre rode

--- script.js
+++ script.js
@@ -10,11 +10,14 @@ function getImg() {
 function displayData(result){
     const box = document.querySelector("#box");
 
-    box.innerHTML = `<img id="anim" src="${result[0].url}"><button id="gen">Generate Cat &#x1F63A;</button>`;
+    box.innerHTML = `<img src="${result[0].url}"><button id="gen">Generate Cat &#x1F63A;</button>`;
 
+    // #gen e img so existem depois de serem criadas na box acima
     const gen = document.querySelector("#gen");
+    const img = document.querySelector("img");
 
     gen.addEventListener("click", getImg);
+    img.onload = () => { img.classList.add("anim") };
 
     box.style.backgroundColor = randColor();
     gen.style.backgroundColor = randColor();
